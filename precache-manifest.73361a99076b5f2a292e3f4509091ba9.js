47b37f5953235523e7da2dd14b64b4cc73849bd2
Gextox <gabriel_il@live.com>
2020-07-14 23:16:13 -0300

    Updates

---
+++ precache-manifest.73361a99076b5f2a292e3f4509091ba9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a96c3ef494cbed9105cfdf0603a7a38",
+    "url": "/Drinks-Recipes/index.html"
+  },
+  {
+    "revision": "a9f08a9e943eec91290c",
+    "url": "/Drinks-Recipes/static/css/main.722f8f3a.chunk.css"
+  },
+  {
+    "revision": "ed728bdb27b25a9f50bf",
+    "url": "/Drinks-Recipes/static/js/2.fdec7d7e.chunk.js"
+  },
+  {
+    "revision": "58aabe32aae325c8d2caf2ff6956fc48",
+    "url": "/Drinks-Recipes/static/js/2.fdec7d7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9f08a9e943eec91290c",
+    "url": "/Drinks-Recipes/static/js/main.b0cb9626.chunk.js"
+  },
+  {
+    "revision": "12706427313257036919",
+    "url": "/Drinks-Recipes/static/js/runtime-main.f74a0518.js"
+  }
+]);
